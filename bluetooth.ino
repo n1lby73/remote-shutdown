6b1fa681522023888605f478067ca2bb2f90a25a
n1lby73 <n1lby73@gmail.com>
2022-08-20 04:58:54 +0100

    changed electric motor pin to match with schematics

--- bluetooth.ino
+++ bluetooth.ino
@@ -2,11 +2,11 @@
 
 SoftwareSerial customSerial(3,4);
 
-int motor = 7;
+int electricMotor = 5;
 int state = 0;
 void setup() {
-  pinMode (motor, OUTPUT);
-  digitalWrite(motor,LOW);
+  pinMode (electricMotor, OUTPUT);
+  digitalWrite(electricMotor,LOW);
   Serial.begin(9600);
 
 }
@@ -16,12 +16,12 @@ void loop() {
     state = customSerial.read();
   }
   if (state == '0'){
-    digitalWrite(motor,LOW);
+    digitalWrite(electricMotor,LOW);
     Serial.println('off');
     state=0; 
   }
   else if (state =='1'){
-    digitalWrite(motor,HIGH);
+    digitalWrite(electricMotor,HIGH);
     Serial.println('off');
     state = 0;
   }
